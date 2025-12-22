402055c314d9e763e975fdeeeff3a7a223c2aebd
Fonzy57 <traben@hotmail.fr>
2025-12-22 09:01:51 +0100

    Initialize Spring Boot project with Maven wrapper, essential configuration files, and a basic structure for testing and application entry point.

---
+++ src/main/java/com/lesartilleursapi/LesArtilleursApiApplication.java
@@ -0,0 +1,13 @@
+package com.lesartilleursapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesArtilleursApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LesArtilleursApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lesartilleursapi/LesArtilleursApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesartilleursapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesArtilleursApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
