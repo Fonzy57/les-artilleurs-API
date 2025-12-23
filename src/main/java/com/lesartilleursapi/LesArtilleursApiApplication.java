ae881183fd0844fa7d82a7b21ac965d202917ea6
Fonzy57 <traben@hotmail.fr>
2025-12-23 11:52:15 +0100

    Add default timezone to UTC

--- src/main/java/com/lesartilleursapi/LesArtilleursApiApplication.java
+++ src/main/java/com/lesartilleursapi/LesArtilleursApiApplication.java
@@ -1,9 +1,14 @@
 package com.lesartilleursapi;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LesArtilleursApiApplication {
 
   public static void main(String[] args) {
@@ -11,6 +16,11 @@ public class LesArtilleursApiApplication {
 
     //TODO SUPPRIMER QUAND TESTS FINIS
     System.out.println("Server started");
+  }
+
+  @PostConstruct
+  public void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
   }
 
 }
