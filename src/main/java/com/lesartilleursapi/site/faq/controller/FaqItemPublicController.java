43f06a697566d398fa3cdd4943faf243810693e5
Fonzy57 <traben@hotmail.fr>
2025-12-26 16:27:15 +0100

    Add `@CrossOrigin` annotation to `FaqItemPublicController` for enabling cross-origin requests

--- src/main/java/com/lesartilleursapi/site/faq/controller/FaqItemPublicController.java
+++ src/main/java/com/lesartilleursapi/site/faq/controller/FaqItemPublicController.java
@@ -3,10 +3,7 @@ package com.lesartilleursapi.site.faq.controller;
 import com.lesartilleursapi.site.faq.model.FaqItem;
 import com.lesartilleursapi.site.faq.service.FaqItemService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +16,7 @@ import java.util.Optional;
  * <p>
  * All endpoints under this controller are publicly accessible and do not require authentication.
  */
+@CrossOrigin
 @RestController
 @RequestMapping("/api/public/site/faq")
 public class FaqItemPublicController {
