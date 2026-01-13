17fb97e6fce80f13ea1d13a5a53a271ed139478f
Fonzy57 <traben@hotmail.fr>
2026-01-13 15:17:01 +0100

    Add javaDoc for FaqItemRepository

--- src/main/java/com/lesartilleursapi/site/faq/repository/FaqItemRepository.java
+++ src/main/java/com/lesartilleursapi/site/faq/repository/FaqItemRepository.java
@@ -4,6 +4,12 @@ import com.lesartilleursapi.site.faq.model.FaqItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing {@link FaqItem} entities.
+ * <p>
+ * Provides standard CRUD operations for FAQ items through {@link JpaRepository}.
+ * This repository is responsible for accessing and persisting FAQ-related data.
+ */
 @Repository
 public interface FaqItemRepository extends JpaRepository<FaqItem, Long> {
 }
