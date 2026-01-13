16270dbd687a59cf6042631c51165c26ae754a1f
Fonzy57 <traben@hotmail.fr>
2026-01-13 16:57:44 +0100

    Add method to find an InfoBlock by display slot in InfoBlockRepository

--- src/main/java/com/lesartilleursapi/site/infoBlock/repository/InfoBlockRepository.java
+++ src/main/java/com/lesartilleursapi/site/infoBlock/repository/InfoBlockRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Repository interface for managing {@link InfoBlock} entities.
@@ -25,4 +26,17 @@ public interface InfoBlockRepository extends JpaRepository<InfoBlock, Long> {
    * @return a list of displayed {@link InfoBlock} entities ordered by slot
    */
   List<InfoBlock> findAllBySlotIsNotNullOrderBySlotAsc();
+
+  /**
+   * Retrieves the info block currently assigned to the given display slot.
+   * <p>
+   * Since display slots are unique, this method returns at most one {@link InfoBlock}.
+   * It is primarily used internally to enforce slot uniqueness when creating
+   * or updating info blocks.
+   *
+   * @param slot the display slot to search for
+   * @return an {@link Optional} containing the {@link InfoBlock} assigned to the slot,
+   * or {@link Optional#empty()} if the slot is not currently in use
+   */
+  Optional<InfoBlock> findBySlot(Integer slot);
 }
