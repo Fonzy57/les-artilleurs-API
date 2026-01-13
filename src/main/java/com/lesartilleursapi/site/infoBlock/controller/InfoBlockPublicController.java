286ab18d110e406d4b97b16861676e0a466b8b61
Fonzy57 <traben@hotmail.fr>
2026-01-13 15:18:50 +0100

    Add method to get all displayed info blocks

--- src/main/java/com/lesartilleursapi/site/infoBlock/controller/InfoBlockPublicController.java
+++ src/main/java/com/lesartilleursapi/site/infoBlock/controller/InfoBlockPublicController.java
@@ -1,10 +1,16 @@
 package com.lesartilleursapi.site.infoBlock.controller;
 
+import com.fasterxml.jackson.annotation.JsonView;
+import com.lesartilleursapi.jsonview.Views;
+import com.lesartilleursapi.site.infoBlock.model.InfoBlock;
 import com.lesartilleursapi.site.infoBlock.service.InfoBlockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -16,5 +22,11 @@ public class InfoBlockPublicController {
   @Autowired
   public InfoBlockPublicController(InfoBlockService infoBlockService) {
     this.infoBlockService = infoBlockService;
+  }
+
+  @GetMapping
+  @JsonView(Views.Public.class)
+  public List<InfoBlock> getAllInfoBlocks() {
+    return infoBlockService.getAllDisplayed();
   }
 }
