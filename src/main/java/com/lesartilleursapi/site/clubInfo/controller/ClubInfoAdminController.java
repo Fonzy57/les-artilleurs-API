14e3c7aa3cf914ffaa1cc06b9d4749ec13d8ecbc
Fonzy57 <traben@hotmail.fr>
2026-01-16 12:24:10 +0100

    Add ClubInfoAdminController for managing admin-specific club information API

---
+++ src/main/java/com/lesartilleursapi/site/clubInfo/controller/ClubInfoAdminController.java
@@ -0,0 +1,13 @@
+package com.lesartilleursapi.site.clubInfo.controller;
+
+//  TODO METTRE ANNOTATION POUR PROTEGER PAR ADMIN
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/admin/club-info")
+public class ClubInfoAdminController {
+}
