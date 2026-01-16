664af3d2cd6d338e94ec05bcaf572c8f736b9564
Fonzy57 <traben@hotmail.fr>
2026-01-16 12:23:42 +0100

    Add ClubInfoPublicController and ClubInfoBaseDto for public API of club information

---
+++ src/main/java/com/lesartilleursapi/site/clubInfo/controller/ClubInfoPublicController.java
@@ -0,0 +1,11 @@
+package com.lesartilleursapi.site.clubInfo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/public/site/club-info")
+public class ClubInfoPublicController {
+}
