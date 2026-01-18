0f8b78f7f10afd1ddd454684a6d513d00ff3fb60
Fonzy57 <traben@hotmail.fr>
2026-01-18 13:18:55 +0100

    Add ClubInfoService with method to fetch club information by ID

--- src/main/java/com/lesartilleursapi/site/clubInfo/service/ClubInfoService.java
+++ src/main/java/com/lesartilleursapi/site/clubInfo/service/ClubInfoService.java
@@ -1,7 +1,31 @@
 package com.lesartilleursapi.site.clubInfo.service;
 
+import com.lesartilleursapi.site.clubInfo.model.ClubInfo;
+import com.lesartilleursapi.site.clubInfo.repository.ClubInfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClubInfoService {
+
+  private final ClubInfoRepository clubInfoRepository;
+
+  @Autowired
+  public ClubInfoService(ClubInfoRepository clubInfoRepository) {
+    this.clubInfoRepository = clubInfoRepository;
+  }
+
+  // --------------
+  // --- PUBLIC ---
+  // --------------
+
+  public Optional<ClubInfo> getClubInfo() {
+    return clubInfoRepository.findById(ClubInfo.getClubInfoId());
+  }
+
+  // -------------
+  // --- ADMIN ---
+  // -------------
 }
