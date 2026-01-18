a57cfd3a29853bede40e6ea8a6f8ed4cfed15a24
Fonzy57 <traben@hotmail.fr>
2026-01-18 13:51:03 +0100

    Add `updateClubInfo` method in `ClubInfoService` to handle creation and updating of club information

--- src/main/java/com/lesartilleursapi/site/clubInfo/service/ClubInfoService.java
+++ src/main/java/com/lesartilleursapi/site/clubInfo/service/ClubInfoService.java
@@ -1,5 +1,6 @@
 package com.lesartilleursapi.site.clubInfo.service;
 
+import com.lesartilleursapi.site.clubInfo.dto.ClubInfoUpsertDto;
 import com.lesartilleursapi.site.clubInfo.model.ClubInfo;
 import com.lesartilleursapi.site.clubInfo.repository.ClubInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +29,24 @@ public class ClubInfoService {
   // -------------
   // --- ADMIN ---
   // -------------
+  public ClubInfo updateClubInfo(ClubInfoUpsertDto dto) {
+    Optional<ClubInfo> clubInfoOpt = clubInfoRepository.findById(ClubInfo.getClubInfoId());
+
+    ClubInfo entity;
+
+    if (clubInfoOpt.isEmpty()) {
+      entity = ClubInfo.createSingleton();
+    } else {
+      entity = clubInfoOpt.get();
+    }
+
+    entity.setClubName(dto.getClubName());
+    entity.setStadiumName(dto.getStadiumName());
+    entity.setStreet(dto.getStreet());
+    entity.setCity(dto.getCity());
+    entity.setPostalCode(dto.getPostalCode());
+    entity.setContactEmail(dto.getContactEmail().trim().toLowerCase());
+
+    return clubInfoRepository.save(entity);
+  }
 }
