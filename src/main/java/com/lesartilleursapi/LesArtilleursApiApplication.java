79bf940504a416aadfd7a4dee5e9691bcfe50c23
Fonzy57 <traben@hotmail.fr>
2026-01-21 10:28:24 +0100

    Add `passwordEncoder` bean using `BCryptPasswordEncoder`

--- src/main/java/com/lesartilleursapi/LesArtilleursApiApplication.java
+++ src/main/java/com/lesartilleursapi/LesArtilleursApiApplication.java
@@ -3,7 +3,10 @@ package com.lesartilleursapi;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.TimeZone;
 
@@ -23,4 +26,8 @@ public class LesArtilleursApiApplication {
     TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
   }
 
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(12);
+  }
 }
