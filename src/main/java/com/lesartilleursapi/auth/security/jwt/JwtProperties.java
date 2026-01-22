e8e080b64eacc4d7cd22be3c22cf7be6806c0ecf
Fonzy57 <traben@hotmail.fr>
2026-01-22 15:21:37 +0100

    Add `JwtProperties` to encapsulate JWT-related configuration properties

---
+++ src/main/java/com/lesartilleursapi/auth/security/jwt/JwtProperties.java
@@ -0,0 +1,13 @@
+package com.lesartilleursapi.auth.security.jwt;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+
+@ConfigurationProperties(prefix = "jwt")
+public record JwtProperties(
+    String secretBase64,
+    Duration accessTokenTtl,
+    String issuer
+) {
+}
