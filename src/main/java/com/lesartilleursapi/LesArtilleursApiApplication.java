a6fcade16c0d4aabfba34f0afde1111ad9eddcf7
Fonzy57 <traben@hotmail.fr>
2026-01-28 10:28:25 +0100

    Enable scheduling in main application configuration

--- src/main/java/com/lesartilleursapi/LesArtilleursApiApplication.java
+++ src/main/java/com/lesartilleursapi/LesArtilleursApiApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
+@EnableScheduling
 public class LesArtilleursApiApplication {
 
   public static void main(String[] args) {
